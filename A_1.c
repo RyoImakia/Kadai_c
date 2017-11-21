9857458273d98a828d4de903e7ae1b4df11c3c00
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-21 14:21:42 +0900

    レビュー直し

---
+++ A_1.c
@@ -0,0 +1,38 @@
+##include <stdio.h>
+
+int Sum(int a, int b, int c){
+
+    int sumSub = a+b+c;
+
+    return sumSub;
+}
+
+int Avarage(int a, int b,int c){
+
+    int avarageSub = (a+b+c)/3;
+    return avarageSub;
+}
+
+int main(void){
+
+    int subjectLn, subjectScience ,subjectSociety ,subjectSum ,subjectAvarage;
+    const char strText[] = "‚Ì“_”‚ğ“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ :";
+    const char subjectTextLn[] =  "‘Œê";
+    const char subjectTextScience[] =  "—‰È";
+    const char subjectTextSociety[] =  "Ğ‰ï";
+
+    printf("%s%s",subjectTextLn, strText);
+    fflush(stdout); scanf("%d\n", &subjectLn);
+
+    printf("%s%s",subjectTextScience , strText);
+    scanf("%d\n", &subjectScience);
+
+    printf("%s%s",subjectTextSociety, strText);
+    scanf("%d\n", &subjectSociety);
+
+    subjectSum = Sum(subjectLn ,subjectScience ,subjectSociety);
+    subjectAvarage = Avarage(subjectLn, subjectScience, subjectSociety);
+
+    printf("‡Œv: %d, •½‹Ï: %d",subjectSum ,subjectAvarage);
+ 
+}
