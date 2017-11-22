523d2dfa6ffd01454ea181ceb5eb705b53e9b8dc
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-22 14:47:24 +0900

    単価×数量の合計と税込み価格の計算のレビュー訂正

--- A_2.c
+++ A_2.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-
-int Sum(int b, int q){
+int CalculationSum(int b, int q){
 
 	int sum = b * q;
 	return sum;
 }
 
-int Tax(int b, int q){
-	
+int CalculationTax(int b, int q){
+
 	int sum = b * q;
-	int tax = sum + (b * 0.08);
+	int tax = sum * 0.08;
+
 	return tax;
 }
 
@@ -20,17 +20,18 @@ int main(void){
 	int b, q, s, t;
 	const char bidText[]      = "単価を入力してください";
 	const char quantityText[] = "数量を入力してください";
-	const char sumText        = "合計";
-	const char taxText        = "税込み金額";
+	const char sumText[]        = "合計";
+	const char taxText[]        = "税込み金額";
 
 	printf("%s", bidText);
-	scanf("%d\n", &b);
+	scanf("%d", &b);
 
 	printf("%s", quantityText);
-    scanf("%d\n", &q);
+	scanf("%d", &q);
 
-	s = Sum(b, q);
-	t = Tax(b, q);
-	printf("%s :%d, %s :%d",sumText, s, taxText, t);
+	s = CalculationSum(b, q);
+	t = CalculationTax(b, q);
+
+	printf("%s :%d %s :%d\n",sumText, s, taxText, s + t);
 
 }
