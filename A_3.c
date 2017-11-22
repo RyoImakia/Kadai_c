50b8d035cd55225bd7332e76370efabffa22163e
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-22 18:26:27 +0900

    子供、大人の人数を足した金額を表示_2(変数名を変更)

--- A_3.c
+++ A_3.c
@@ -5,13 +5,13 @@ int main(void){
 	int childNum, adultNum;
 	const int childPrice = 800;
 	const int adultPrice = 1500;
-	const char childPriceText[] = "子供の人数を入力してください";
-	const char adultPriceText[] = "大人の人数を入力してください";
+	const char childPriceText[] = "子供の人数を入力してください: ";
+	const char adultPriceText[] = "大人の人数を入力してください: ";
 	const char priceTtext[] = "金額";
 
-	printf("%s", childPriceText); scanf("%d", &childNum);
-	printf("%s", adultPriceText); scanf("%d", &adultNum);
+	printf("%s", childPriceText); fflush(stdout); scanf("%d", &childNum);
+	printf("%s", adultPriceText); fflush(stdout); scanf("%d", &adultNum);
 
-	int sum = (cn * childPrice) + (an * adultPrice);
+	int sum = (childNum * childPrice) + (adultNum * adultPrice);
 	printf("%s: %d\n", priceTtext, sum);
 }
