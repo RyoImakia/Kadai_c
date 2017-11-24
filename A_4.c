7851e15a84584d944cddda9166ce20210112294c
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-24 14:03:22 +0900

    ４つあるprintfをまとめる、変数命名訂正

--- A_4.c
+++ A_4.c
@@ -4,25 +4,33 @@ int main (void){
 
 	const char injusticeText[]  = "不正な値が入力されました。";
 	const char scoreText[] = "点数を入力してください: ";
-	const char JudgmentText[] = "判定です。";
+	const char judgmentText[] = "判定です。";
 	const char a = 'A';
 	const char b = 'B';
-	const char c = 'c';
+	const char c = 'C';
+	char answer = 'x';
 	int score;
 
 	printf("%s", scoreText); fflush(stdout); scanf("%d", &score);
 	puts("");
 
-	if(score > 100 || score < 0){
-		printf("%s\n", injusticeText);
-	}
-	else if(score >= 80 && score <= 100){
-		printf("%c%s\n", a, JudgmentText);
+	if(score >= 80 && score <= 100){
+		answer = a;
+		
 	}
 	else if(score <= 79 && score >= 60){
-		printf("%c%s\n", b, JudgmentText);
+		answer = b;
+		
 	}
-	else{
-		printf("%c%s\n", c, JudgmentText);
+	else if(score <= 59 && score >= 0){
+		answer = c;
+		
+	}
+
+	if(answer == 'x'){
+		printf("%s", injusticeText);
+	}else{
+		printf("%c%s", answer, judgmentText);
+
 	}
 }
