11c6548ae28a0f52edf0e64d606a86d91ced1022
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-28 18:14:21 +0900

    レビュー訂正(変数名の変更)

--- A_6.c
+++ A_6.c
@@ -2,23 +2,23 @@
 
 int main(void){
 
-	const char classIfiCationOne[] = "区分1の人数を入力してください: ";
-	const char classIfiCationTwo[] = "区分2の人数を入力してください: ";
-	const char classIfiCationThree[] = "区分3の人数を入力してください: ";
+	const char categoryOne[] = "区分1の人数を入力してください: ";
+	const char categoryTwo[] = "区分2の人数を入力してください: ";
+	const char categoryThree[] = "区分3の人数を入力してください: ";
 	const char priceText[] = "金額: ";
 
-	const int classIfiCationOnePrice = 800;
-	const int classIfiCationTwoPrice = 1000;
-	const int classIfiCationThreePrice = 1500;
+	const int categoryOnePrice = 800;
+	const int categoryTwoPrice = 1000;
+	const int categoryThreePrice = 1500;
 
 	int oneWard, twoWard, threeWard;
 	int sum = 0;
 
-	printf("%s", classIfiCationOne); scanf("%d", &oneWard);
-	printf("%s", classIfiCationTwo); scanf("%d", &twoWard);
-	printf("%s", classIfiCationThree); scanf("%d", &threeWard);
+	printf("%s", categoryOne); fflush(stdout); scanf("%d", &oneWard);
+	printf("%s", categoryTwo); fflush(stdout); scanf("%d", &twoWard);
+	printf("%s", categoryThree); fflush(stdout); scanf("%d", &threeWard);
 
-	sum += (classIfiCationOnePrice * oneWard) + (classIfiCationTwoPrice * twoWard) + (classIfiCationThreePrice * threeWard);
+	sum += (categoryOnePrice * oneWard) + (categoryTwoPrice * twoWard) + (categoryThreePrice * threeWard);
 
 	printf("%s%d\n", priceText, sum);
 }
