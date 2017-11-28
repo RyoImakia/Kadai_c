0d1b61dfbdbe900c51ac09f40b18edfb57e650b6
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-28 15:16:50 +0900

    料金区分と人数を入力する｡料金区分が１のとき料金を800円、２のとき1000円、３ のとき1500円とし、それに人数を掛けて、金額を算出する。料金と金額を表示する｡

---
+++ A_6.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void){
+
+	const char classIfiCationOne[] = "区分1の人数を入力してください: ";
+	const char classIfiCationTwo[] = "区分2の人数を入力してください: ";
+	const char classIfiCationThree[] = "区分3の人数を入力してください: ";
+	const char priceText[] = "金額: ";
+
+	const int classIfiCationOnePrice = 800;
+	const int classIfiCationTwoPrice = 1000;
+	const int classIfiCationThreePrice = 1500;
+
+	int oneWard, twoWard, threeWard, sum;
+
+	for(int i = 0; i < 3; i++){
+		printf("%s", classIfiCationOne); scanf("%d", &oneWard);
+		printf("%s", classIfiCationTwo); scanf("%d", &twoWard);
+		printf("%s", classIfiCationThree); scanf("%d", &threeWard);
+
+		sum += (classIfiCationOnePrice * oneWard) + (classIfiCationTwoPrice * twoWard) + (classIfiCationThreePrice * threeWard);
+	}
+	printf("%s%d\n", priceText, sum);
+}
