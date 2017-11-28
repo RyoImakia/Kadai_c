babf587a81ade6d99c06937fe3dac1e8ee5c27d7
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-28 09:24:03 +0900

    レビュー訂正(if分の配置変更とstrncpyの所を関数化

--- A_5.c
+++ A_5.c
@@ -1,56 +1,76 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdlib.h>
 
 typedef struct{
 
 	char name[10];
-	int point;
+	int  point;
 
 }man;
 
+int ParserKino(man *tempKino, char *kinoArray){
+
+	char *kinoCommaPosition;
+	char kinoCp[10];
+	int kinoArraySize;
+	char comma[] = ",";
+
+	kinoCommaPosition = strstr(kinoArray, comma);
+	kinoArraySize = sizeof(kinoArray) / sizeof(kinoArray[0]);
+
+	//kinoArraySize = "8", kinoCommaPosition = "4", 数値はカンマの手前("4")から空白を挟んで二つ右なので最後に"+2"
+	strncpy(kinoCp, kinoArray + (kinoArraySize - (kinoCommaPosition - kinoArray) + 2), 1);
+
+	//名前は配列先頭アドレスからカンマの手前("4")まで必要
+	strncpy(tempKino->name, kinoArray, kinoCommaPosition - kinoArray);
+
+	tempKino->point = atoi(kinoCp);
+
+	return 0;
+}
+
+int ParserKuno(man *tempKuno , char *kunoArray){
+
+	char *kunoCommaPosition;
+	char kunoCp[10];
+	int kunoArraySize;
+	char comma[] = ",";
+
+	kunoCommaPosition = strstr(kunoArray, comma);
+	kunoArraySize = sizeof(kunoArray) / sizeof(kunoArray[0]);
+
+	//kinoArraySize = "8", kinoCommaPosition = "4", 数値はカンマの手前("4")から空白を挟んで二つ右なので最後に"+2"
+	strncpy(kunoCp, kunoArray + (kunoArraySize - (kunoCommaPosition - kunoArray) + 2), 1);
+
+	//名前は配列先頭アドレスからカンマの手前("4")まで必要
+	strncpy(tempKuno->name, kunoArray, kunoCommaPosition - kunoArray);
+
+	tempKuno->point = atoi(kunoCp);
+	return 0;
+}
+
 int main(void){
 
-	man kino, kuno = {'\0', 0};
+	man kino, kuno = {{'\0'}, 0};
 
 	const char nameText[] = "名前、";
 	const char pointText[] = "点数を入力してください :";
 	const char winText[] = "の勝ち";
 	const char drawText[] = "引き分け";
 
-	char kinoArray[10], kinoCp[10] ;
-	char kunoArray[10], kunoCp[10] ;
-	char kanma[] = ",";
-	int kinoArraySize, kunoArraySize;
-	char *kinoKanmaPosition, *kunoKanmaPosition;
+	char kinoArray[10], kunoArray[10];
 
 	//テキスト表示と標準入力の実行
 	printf("%s%s", nameText, pointText);
-	fgets(kinoArray, sizeof(kinoArray), stdin);
-
-	//構造体 "kino"
-	kinoKanmaPosition = strstr(kinoArray, kanma);
-	kinoArraySize = sizeof(kinoArray) / sizeof(kinoArray[0]);
-
-	strncpy(kinoCp, kinoArray + (kinoArraySize - (kinoKanmaPosition - kinoArray)), 1);
-	strncpy(kino.name, kinoArray, kinoKanmaPosition - kinoArray);
-
-	kino.point = atoi(kinoCp);
-
+	fflush(stdout); fgets(kinoArray, sizeof(kinoArray), stdin);
 
 	//テキスト表示と標準入力の実行
 	printf("%s%s", nameText, pointText);
-	fgets(kunoArray, sizeof(kunoArray), stdin);
+	fflush(stdout); fgets(kunoArray, sizeof(kunoArray), stdin);
 
-	//構造体 "kuno"
-	kunoKanmaPosition = strstr(kunoArray, kanma);
-	kunoArraySize = sizeof(kunoArray) / sizeof(kunoArray[0]);
-
-
-	strncpy(kunoCp, kunoArray + (kunoArraySize - (kunoKanmaPosition - kunoArray)), 1);
-	strncpy(kuno.name, kunoArray, kunoKanmaPosition - kunoArray);
-
-	kuno.point = atoi(kunoCp);
+	//関数に構造体 "kino","kuno"と標準入力の配列のアドレスを渡す
+	ParserKino(&kino, &kinoArray);
+	ParserKuno(&kuno, &kunoArray);
 
 
 	if(kino.point == kuno.point){
