0719d95dc1d45bd90ba6b6f53c0826c09adbcf79
Ryo Imakita <i.rkyaon@gmail.com>
2017-11-28 14:09:14 +0900

    レビュー訂正(関数の仕様変更、変数と関数の名前変更)

--- A_5.c
+++ A_5.c
@@ -6,67 +6,54 @@ typedef struct{
 	char name[10];
 	int  point;
 
-}man;
 
+}Man;
 
-void ParserArray(man *tempKuno, man *tempKino , char *kunoArray, char *kinoArray){
+void ArrayUnplug(Man *proxyStructure, char *proxyCase){
 
 	char comma[] = ",";
 
-	char *kunoCommaPosition, *kinoCommaPosition;
-	char kunoCp[10], kinoCp[10];
-	int kunoArraySize, kinoArraySize;
+	char *commaPosition;
+	char caseCopy[10];
+	int caseSize;
 
-	//構造体kuno
 
-	kunoCommaPosition = strstr(kunoArray, comma);
-	kunoArraySize = sizeof(kunoArray) / sizeof(kunoArray[0]);
+	commaPosition = strstr(proxyCase, comma);
+	caseSize = sizeof(proxyCase) / sizeof(proxyCase[0]);
 
-	//kunoArraySize = "8", kunoCommaPosition = "4", 数値はカンマの手前("4")から空白を挟んで二つ右なので最後に"+2"
-	strncpy(kunoCp, kunoArray + (kunoArraySize - (kunoCommaPosition - kunoArray) + 2), 1);
-
-	//名前は配列先頭アドレスからカンマの手前("4")まで必要
-	strncpy(tempKuno->name, kunoArray, kunoCommaPosition - kunoArray);
-
-	tempKuno->point = atoi(kunoCp);
-
-	//構造体kino
-
-	kinoCommaPosition = strstr(kinoArray, comma);
-	kinoArraySize = sizeof(kinoArray) / sizeof(kinoArray[0]);
-
-	//kinoArraySize = "8", kinoCommaPosition = "4", 数値はカンマの手前("4")から空白を挟んで二つ右なので最後に"+2"
-	strncpy(kinoCp, kinoArray + (kinoArraySize - (kinoCommaPosition - kinoArray) + 2), 1);
+	//caseSize = "8", commaPosition = "4", 数値はカンマの手前("4")から空白を挟んで二つ右なので最後に"+2"
+	strncpy(caseCopy, proxyCase + (caseSize - (commaPosition - proxyCase) + 2), 1);
 
 	//名前は配列先頭アドレスからカンマの手前("4")まで必要
-	strncpy(tempKino->name, kinoArray, kinoCommaPosition - kinoArray);
+	strncpy(proxyStructure->name, proxyCase, commaPosition - proxyCase);
 
-	tempKino->point = atoi(kinoCp);
+	proxyStructure->point = atoi(caseCopy);
 
 }
 
 int main(void){
 
-	man kino, kuno = {{'\0'}, 0};
+	Man kino, kuno = {{'\0'}, 0};
 
 	const char nameText[] = "名前、";
 	const char pointText[] = "点数を入力してください :";
-	const char winText[] = "の勝ち";
-	const char drawText[] = "引き分け";
+	const char winText[] = "の勝ち: ";
+	const char drawText[] = "引き分け: ";
 
-	char kinoArray[10], kunoArray[10];
-
-	//テキスト表示と標準入力の実行
-	printf("%s%s", nameText, pointText);
-	fgets(kinoArray, sizeof(kinoArray), stdin);
+	char kinoCase[10], kunoCase[10];
 
 	//テキスト表示と標準入力の実行
 	printf("%s%s", nameText, pointText);
-	fgets(kunoArray, sizeof(kunoArray), stdin);
+	fflush(stdout); fgets(kinoCase, sizeof(kinoCase), stdin);
+	//関数に構造体 "kuno"と標準入力の配列のアドレスを渡す
+	ArrayUnplug(&kino, &kinoCase);
 
-	//関数に構造体 "kino","kuno"と標準入力の配列のアドレスを渡す
 
-	ParserArray(&kuno, &kino, &kunoArray, &kinoArray);
+	//テキスト表示と標準入力の実行
+	printf("%s%s", nameText, pointText);
+	fflush(stdout); fgets(kunoCase, sizeof(kunoCase), stdin);
+	//関数に構造体 "kino"と標準入力の配列のアドレスを渡す
+	ArrayUnplug(&kuno, &kunoCase);
 
 
 	if(kino.point == kuno.point){
